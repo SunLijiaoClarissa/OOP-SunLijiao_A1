0b53708c873f815dc3939946c333c695e7efd699
SunLijiaoClarissa <2923783519@qq.com>
2024-11-19 12:14:21 +0800

    Added clinic location function for ophthalmologists
    Added clinic location function for ophthalmologists

--- src/Ophthalmologist.java
+++ src/Ophthalmologist.java
@@ -1,32 +1,49 @@
 public class Ophthalmologist extends HealthProfessional {
-    private String qualification;  // 资格证书
+    private String qualification;   // 资格证书
+    private String clinicLocation;  // 诊所位置
 
     // 默认构造函数
     public Ophthalmologist() {
-        super();  // 调用父类的默认构造函数
+        super();
         this.qualification = "";
+        this.clinicLocation = "";
     }
 
-    // 第二个构造函数，初始化所有实例变量
-    public Ophthalmologist(int id, String name, String info, String contactEmail, String qualification) {
-        super(id, name, info, contactEmail);  // 调用父类的带参构造函数
+    // 带参构造函数
+    public Ophthalmologist(int id, String name, String info, String contactEmail, String qualification, String clinicLocation) {
+        super(id, name, info, contactEmail);
         this.qualification = qualification;
+        this.clinicLocation = clinicLocation;
     }
 
-    // 打印医生的详细信息，重写父类的 printDetails 方法
-    @Override
-    public void printDetails() {
-        System.out.println("The health professional details are:");
-        super.printDetails();  // 打印父类（HealthProfessional）的信息
-        System.out.println("Qualification: " + qualification);  // 打印该医生的资格证书
-    }
-
-    // Getter 和 Setter 方法
+    // Getter 和 Setter
     public String getQualification() {
         return qualification;
     }
 
     public void setQualification(String qualification) {
         this.qualification = qualification;
+    }
+
+    public String getClinicLocation() {
+        return clinicLocation;
+    }
+
+    public void setClinicLocation(String clinicLocation) {
+        this.clinicLocation = clinicLocation;
+    }
+
+    // 判断是否在特定地点工作
+    public boolean isWorkingAt(String location) {
+        return clinicLocation.equals(location);
+    }
+
+    // 打印详细信息
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Qualification  : " + qualification);
+        System.out.println("Clinic Location: " + clinicLocation);
+        System.out.println("------------------------------");
     }
 }
